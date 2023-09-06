2da4b09e020f0f434460da2923669fde231f875c
ManuelBarrabino <124201255+ManuelBarrabino@users.noreply.github.com>
2023-09-05 21:27:43 -0300

    Update pre_gen.dart

--- feature_route_bloc/hooks/pre_gen.dart
+++ feature_route_bloc/hooks/pre_gen.dart
@@ -5,18 +5,18 @@ void run(HookContext context) async {
   try {
     //Add feature path to template
     final directory = Directory.current.path;
-    final folders = directory.toString().split('\').toList();
+    final folders = directory.toString().split('\\').toList();
     final index = folders.indexWhere((folder) => folder == 'lib');
     var featurePath =
-        folders.sublist(index + 1, folders.length).join('\').toString();
+        folders.sublist(index + 1, folders.length).join('\\').toString();
     final featureName = context.vars['feature_name'];
-    if (featurePath.isNotEmpty) featurePath += '\';
+    if (featurePath.isNotEmpty) featurePath += '\\';
     context.vars['path'] = featurePath + featureName;
 
     //Add flutter_bloc & equatable to pubspec
     final addDependencies = context.vars['add_dependencies'];
     if (addDependencies) {
-      final root = folders.sublist(0, index).join('\').toString();
+      final root = folders.sublist(0, index).join('\\').toString();
       final progress = context.logger.progress(
         'Adding dependencies flutter_bloc equatable',
       );
